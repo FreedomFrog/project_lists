66e5a74075af0c2bb1a6bc809941b2a4b1109c71
Freedomfrog <anthony.jaeger@gmail.com>
2019-01-12 16:41:58 -0800

    changes to the models websitelink

--- oregonwine/models.py
+++ oregonwine/models.py
@@ -7,11 +7,14 @@ class Winery(models.Model):
     ava = models.ForeignKey('AVA', on_delete=models.CASCADE,)
     phone = models.CharField(max_length=12)
     email = models.EmailField()
-    website = models.CharField(max_length=100)
+    websitelink = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
 
 class AVA(models.Model):
     name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
 
